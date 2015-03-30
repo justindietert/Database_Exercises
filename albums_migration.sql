c1cda21d2a5bcf74e7d8cd1352102132fd1cfe5f
Justin Dietert <justin.dietert@gmail.com>
2015-03-30 09:32:12 -0500

    removed whitespace in file

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE albums (
     artist VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
     release_date YEAR(4) NOT NULL,
-    sales    FLOAT(11,2) NOT NULL,
+    sales FLOAT(11,2) NOT NULL,
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );

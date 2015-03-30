58b044c5623050fc4e2e9e390bf21b063b90f157
Justin Dietert <justin.dietert@gmail.com>
2015-03-30 13:51:47 -0500

    changed YEAR to INT

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    release_date YEAR(4) NOT NULL,
+    release_date INT(4) UNSIGNED NOT NULL,
     sales_in_millions FLOAT(11,2) NOT NULL,
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)

010e465c315e70b277b05f5423305c36f2d9f708
Justin Dietert <justin.dietert@gmail.com>
2015-03-30 14:07:45 -0500

    deleted selections

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 
 -- Albums released after 1991
 SELECT 'Released after 1991' AS 'Albums';
-SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 -- Albums with the genre "disco"
 SELECT 'Disco' AS 'Album genre:';
-SELECT * FROM albums WHERE genre = 'disco';
+DELETE FROM albums WHERE genre = 'disco';
 -- Albums by "Whitney Houston"
 SELECT 'Whitney Houston' AS 'Albums by';
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist = 'Whitney Houston';

1441538519ef4769f74227b6679fb6735f24a673
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 10:11:22 -0500

    cleaned up queries for readability

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,26 +1,26 @@
 SELECT * FROM employees
-WHERE (first_name = 'Irena'
-   OR first_name = 'Vidya'
-   OR first_name = 'Maya')
-  AND (gender = 'M');
+ WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+   AND (gender = 'M');
 
 SELECT * FROM employees 
-WHERE last_name 
- LIKE 'E%' 
-   OR last_name 
- LIKE '%e';
+ WHERE last_name 
+  LIKE 'E%' 
+    OR last_name 
+  LIKE '%e';
  
 SELECT * FROM employees 
-WHERE last_name 
- LIKE 'E%' 
-  AND last_name 
- LIKE '%e';
+ WHERE last_name 
+  LIKE 'E%' 
+   AND last_name 
+  LIKE '%e';
 
 SELECT * FROM employees 
-WHERE hire_date 
- LIKE '199%'
-  AND birth_date 
- LIKE '%-12-25';
+ WHERE hire_date 
+  LIKE '199%'
+   AND birth_date 
+  LIKE '%-12-25';
 
 SELECT * FROM employees 
  WHERE last_name LIKE '%q%'

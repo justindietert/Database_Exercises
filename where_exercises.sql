c5dcc2864fefab8ac92c6ab4053b8ced56c7fc16
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 09:52:22 -0500

    added condition to find those who are also male

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,7 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
@@ -7,3 +10,4 @@ SELECT * FROM employees WHERE hire_date LIKE '199%';
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
+

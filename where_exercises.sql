fefe7b9c5172fe8951c091766be997eb8629f0fb
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 09:30:00 -0500

    employees born on Christmas query added

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,3 +3,5 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees WHERE hire_date LIKE '199%';
+
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25';

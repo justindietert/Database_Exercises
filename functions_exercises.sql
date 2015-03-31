d968c624f0dcfc5449fe94ce58f7250469e846f8
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 15:13:09 -0500

    completed functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,14 +18,18 @@ SELECT * FROM employees
    AND last_name 
   LIKE '%e';
 
-SELECT * FROM employees 
+SELECT first_name, last_name, datediff(curdate(), hire_date) 
+  FROM employees
  WHERE hire_date 
   LIKE '199%'
    AND birth_date 
   LIKE '%-12-25'
   ORDER BY birth_date ASC, hire_date DESC;
 
-SELECT * FROM employees 
+SELECT first_name, last_name, count(*) AS 'Number of People with this Name'
+  FROM employees 
  WHERE last_name LIKE '%q%'
-   AND last_name NOT LIKE '%qu%';
+   AND last_name NOT LIKE '%qu%'
+   GROUP BY first_name, last_name
+   ORDER BY count(*) DESC;
 

57c718e2f56523d1aadfb2535e2e4b3a0fab68d9
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 10:03:15 -0500

    Find all employees hired in the 90s and born on Christmas

--- where_exercises.sql
+++ where_exercises.sql
@@ -16,7 +16,11 @@ WHERE last_name
   AND last_name 
  LIKE '%e';
 
-SELECT * FROM employees WHERE hire_date LIKE '199%';
+SELECT * FROM employees 
+WHERE hire_date 
+ LIKE '199%'
+  AND birth_date 
+ LIKE '%-12-25';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
 

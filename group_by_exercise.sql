2be7bbe06e856553f3946b86a87e49b33061c475
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 11:49:04 -0500

    Find the unique last names with a 'q' but not 'qu'

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -5,3 +5,10 @@ SELECT first_name, last_name FROM employees
  WHERE last_name LIKE 'E%' 
    AND last_name LIKE '%e'
 GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees 
+ WHERE last_name LIKE '%q%'
+   AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+   
+   

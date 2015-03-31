c0e973d08b3cbb8432923f92c0b2e66858ad4f5c
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 09:31:38 -0500

    query for employees whose last name contains 'q'

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,3 +5,5 @@ SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE hire_date LIKE '199%';
 
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';

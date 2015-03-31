5289842d22819617362376d5608d0a7ba262091e
Justin Dietert <justin.dietert@gmail.com>
2015-03-31 10:00:25 -0500

    added query to find employees whose last name start AND ends with 'e'

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,9 +1,20 @@
 SELECT * FROM employees
-WHERE first_name = 'Irena'
+WHERE (first_name = 'Irena'
    OR first_name = 'Vidya'
-   OR first_name = 'Maya';
+   OR first_name = 'Maya')
+  AND (gender = 'M');
 
-SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees 
+WHERE last_name 
+ LIKE 'E%' 
+   OR last_name 
+ LIKE '%e';
+ 
+SELECT * FROM employees 
+WHERE last_name 
+ LIKE 'E%' 
+  AND last_name 
+ LIKE '%e';
 
 SELECT * FROM employees WHERE hire_date LIKE '199%';
 
